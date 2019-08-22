f7c0887a136a0ffc69d71dd989aa85c5b1b15514
unknown <civoremon@gmail.com>
2019-08-22 11:07:08 -0400

    about me section

--- my_portfolio/src/App.js
+++ my_portfolio/src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import "./App.css";
 
 import NavBar from "./components/navbar";
-import MainHeader from './components/mainHeader'
+import MainHeader from './components/mainHeader';
+import AboutMe from './components/aboutme';
 
 function App() {
 	return (
 		<div className="App">
 			<NavBar />
 			<MainHeader />
+			<AboutMe />
 			{/* <p>This is temp text</p> */}
 		</div>
 	);
---
+++ my_portfolio/src/components/aboutme.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useRef } from 'react';
+import { TweenMax, Linear, TimelineLite, Back } from 'gsap';
+import Container from 'react-bootstrap/Container';
+import styled from 'styled-components';
+
+const AboutMe = () => {
+
+
+    return (
+        <div>Some About me Text</div>
+
+    )
+
+}
+
+export default AboutMe;
