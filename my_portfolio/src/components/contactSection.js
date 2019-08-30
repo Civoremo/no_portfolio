8b6144c5439cfba1cd8826e748fa96d01e5d0e4b
unknown <civoremon@gmail.com>
2019-08-30 10:13:39 -0400

    input fields added

--- my_portfolio/src/components/contactSection.js
+++ my_portfolio/src/components/contactSection.js
@@ -9,6 +9,10 @@ const ContactContainerDiv = styled.div `
     padding: 50px 0 50px 0;
 `
 
+const FormContainer = styled.div `
+    max-width: 650px;
+`
+
 const ContactForm = () => {
 
     return (
@@ -19,7 +23,31 @@ const ContactForm = () => {
                         Get in Contact
                     </span>
                 </h2>
-                contact form section
+                <Container style={{border: '1px solid red', padding: '30px 70px'}}>
+                    <FormContainer>
+                        <form action='/contact_page.php' style={{paddingRight: '25px', paddingBottom: '30px', borderRight: '5px solid red'}}>
+                            <div className='form-group' >
+                                <div className='form-row'>
+                                    <div className='col' style={{minWidth: '300px', margin: '15px 0 15px 15px'}}>
+                                        <label for='formGroupNameInput' style={{ textAlign: 'left', width: '100%', fontFamily: 'Open Sans Condensed', fontWeight: 'bold', fontSize: '22px', letterSpacing: '5px', padding: '0 20px'}}>Name</label>
+                                        <input type='text' className='form-control' id='formGroupNameInput' required ></input>
+                                    </div>
+                                    <div className='col' style={{minWidth: '300px', margin: '15px 0 15px 15px'}}>
+                                        <label for='formGroupEmailInput' style={{ textAlign: 'left', width: '100%', fontFamily: 'Open Sans Condensed', fontWeight: 'bold', fontSize: '22px', letterSpacing: '5px', padding: '0 20px'}}>Email</label>
+                                        <input type='text' className='form-control' id='formGroupEmailInput' required></input>
+                                    </div>
+                                </div>
+                                <div className='form-row'>
+                                    <div className='col' style={{minWidth: '300px', margin: '15px 0 15px 15px'}}>
+                                        <label for='formGroupMessageInput' style={{ textAlign: 'left', width: '100%', fontFamily: 'Open Sans Condensed', fontWeight: 'bold', fontSize: '22px', letterSpacing: '5px', padding: '0 20px'}}>Message</label>
+                                        <textarea type='text' className='form-control' id='formGroupMessageInput' required rows='6' style={{resize: 'none'}}></textarea>
+                                    </div>
+                                </div>
+                            </div>
+                            <button type="button" className='btn btn-danger'>Submit</button>
+                        </form>
+                    </FormContainer>
+                </Container>
             </Container>
         </ContactContainerDiv>
     )
