3097e57b3ec2dba358ee1d5abee7a8fd73678ff6
Civoremo <civoremon@gmail.com>
2021-08-11 19:41:52 -0400

    additional projects show modal and display image carousel and extended info

--- my_portfolio/src/components/Projects/moreProjects.js
+++ my_portfolio/src/components/Projects/moreProjects.js
@@ -4,19 +4,23 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 import Flip from "react-reveal/Flip";
 import Modal from "react-bootstrap/Modal";
+import Carousel from "react-bootstrap/Carousel";
 import {
-  EachCardDiv,
+  // EachCardDiv,
   EachCardWrapperDiv,
   ModalLink,
-  CarouselImage,
+  // CarouselImage,
 } from "./styledCompProjects";
 
 const MoreProjects = () => {
-  const [lgProjects, setLgProjects] = useState(false);
+  const [lgProject, setLgProject] = useState(false);
   const [projectsData, setProjectsData] = useState(null);
   const [projectGroupIndex, setProjectGroupIndex] = useState(0);
   const [projectIndexes, setProjectIndexes] = useState({});
+  const [selectedProjectId, setSelectedProjectId] = useState(null);
+  const [extendedProjectInfo, setExtendedProjectInfo] = useState(null);
 
+  // load projects
   useEffect(() => {
     axios({
       method: "get",
@@ -43,6 +47,35 @@ const MoreProjects = () => {
     });
   }, []);
 
+  // render quantity of project cards based on index
+  useEffect(() => {
+    if (projectsData !== null) {
+      displayProjectCards();
+    }
+  }, [projectGroupIndex]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  // extended project info
+  useEffect(() => {
+    if (selectedProjectId !== null) {
+      axios({
+        method: "get",
+        url: `${process.env.REACT_APP_API_URL}/projects/info/${selectedProjectId}`,
+        responseType: "json",
+      })
+        .then(result => {
+          // console.log("extended info", result);
+          if (result.status === 200) {
+            setExtendedProjectInfo(result.data);
+          } else {
+            console.log("Something went wrong fetching extended project info.");
+          }
+        })
+        .catch(err => {
+          console.log("failed to fetch extended info");
+        });
+    }
+  }, [selectedProjectId]);
+
   const displayProjectData = project => {
     return (
       <EachCardWrapperDiv style={{ margin: "10px 10px 85px 10px" }}>
@@ -68,7 +101,15 @@ const MoreProjects = () => {
               <p className='card-text' style={{ minHeight: "6rem" }}>
                 {project.description}
               </p>
-              <ModalLink>Learn more</ModalLink>
+              <ModalLink
+                onClick={event => {
+                  setLgProject(true);
+                  setSelectedProjectId(project.id);
+                }}
+              >
+                Learn more
+              </ModalLink>
+              {displayModal()}
             </div>
             <div
               className='card-body'
@@ -130,39 +171,160 @@ const MoreProjects = () => {
   };
 
   const displayProjectCards = () => {
-    return projectsData.map((project, index) => {
-      if (index % 2 === 0) {
-        return (
-          <Flip key={project.id} top delay={index * 100}>
-            <div
-              style={{
-                display:
-                  projectIndexes[project.id] <= projectGroupIndex
-                    ? "block"
-                    : "none",
-              }}
-            >
-              {displayProjectData(project)}
+    if (projectsData !== null) {
+      return projectsData.map((project, index) => {
+        if (index % 2 === 0) {
+          return (
+            <Flip key={project.id} top delay={index * 100}>
+              <div
+                style={{
+                  display:
+                    projectIndexes[project.id] <= projectGroupIndex
+                      ? "block"
+                      : "none",
+                }}
+              >
+                {displayProjectData(project)}
+              </div>
+            </Flip>
+          );
+        } else {
+          return (
+            <Flip key={project.id} bottom delay={index * 100}>
+              <div
+                style={{
+                  display:
+                    projectIndexes[project.id] <= projectGroupIndex
+                      ? "block"
+                      : "none",
+                }}
+              >
+                {displayProjectData(project)}
+              </div>
+            </Flip>
+          );
+        }
+      });
+    }
+  };
+
+  const displayModal = () => {
+    if (selectedProjectId === null && extendedProjectInfo === null) {
+      return (
+        <Modal
+          size='lg'
+          show={lgProject}
+          onHide={event => {
+            setLgProject(false);
+            setSelectedProjectId(null);
+            setExtendedProjectInfo(null);
+          }}
+          aria-labelledby='example-modal-sizes-title-lg'
+          scrollable='true'
+        >
+          <Modal.Body>Loading ...</Modal.Body>
+        </Modal>
+      );
+    } else {
+      let projectInfo = projectsData.filter(
+        project => project.id === selectedProjectId
+      );
+
+      return (
+        <Modal
+          size='lg'
+          show={lgProject}
+          onHide={event => {
+            setLgProject(false);
+            setSelectedProjectId(null);
+            setExtendedProjectInfo(null);
+          }}
+          aria-labelledby='example-modal-sizes-title-lg'
+          scrollable='true'
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id='example-modal-sizes-title-lg'>
+              {projectInfo[0].title}
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <div>
+              <div>{displayCarousel()}</div>
+              <div style={{ margin: "30px 20px" }}>{displayExtendedInfo()}</div>
             </div>
-          </Flip>
-        );
-      } else {
-        return (
-          <Flip key={project.id} bottom delay={index * 100}>
-            <div
-              style={{
-                display:
-                  projectIndexes[project.id] <= projectGroupIndex
-                    ? "block"
-                    : "none",
-              }}
-            >
-              {displayProjectData(project)}
-            </div>
-          </Flip>
-        );
-      }
-    });
+          </Modal.Body>
+        </Modal>
+      );
+    }
+  };
+
+  const displayCarousel = () => {
+    if (extendedProjectInfo === null) {
+      return <>Loading ...</>;
+    } else {
+      return (
+        <Carousel
+          pause={true}
+          interval={2000}
+          slide={false}
+          style={{
+            backgroundColor: "#222",
+            height: "400px",
+          }}
+        >
+          {extendedProjectInfo[1].map((image, index) => {
+            return (
+              <Carousel.Item
+                key={image.id}
+                style={{
+                  maxHeight: "400px",
+                }}
+              >
+                <div style={{ display: "flex", justifyContent: "center" }}>
+                  <img
+                    width='auto'
+                    height={400}
+                    src={extendedProjectInfo[1][index].image}
+                    // className='d-block w-100'
+                    alt={extendedProjectInfo[1][index].id + " carousel image"}
+                  />
+                </div>
+              </Carousel.Item>
+            );
+          })}
+        </Carousel>
+      );
+    }
+  };
+
+  const displayExtendedInfo = () => {
+    let mainProjectInfo = projectsData.filter(
+      project => project.id === selectedProjectId
+    );
+    if (extendedProjectInfo === null) {
+      return <>Loading ...</>;
+    } else {
+      return (
+        <div>
+          <div
+            style={{
+              display: extendedProjectInfo[0].link !== null ? "block" : "none",
+            }}
+          >
+            <a href={extendedProjectInfo[0].link}>Click for Additional Info</a>
+          </div>
+          <br />
+          <div>
+            <h5>Stack</h5>
+            <div>{mainProjectInfo.stack}</div>
+          </div>
+          <hr />
+          <div>
+            <p>{extendedProjectInfo[0].description}</p>
+          </div>
+        </div>
+      );
+    }
   };
 
   if (projectsData === null) {
@@ -177,7 +339,6 @@ const MoreProjects = () => {
         justifyContent: "space-around",
       }}
     >
-      {console.log("indexes", projectIndexes)}
       {displayProjectCards()}
     </div>
   );
